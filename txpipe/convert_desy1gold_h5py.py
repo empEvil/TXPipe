31a29cdf7ce05d380717df0d755c924a48b531e0
Judit Prat <jprat@cori03.nersc.gov>
2019-11-20 13:28:09 -0800

    Adding new version of script for converting to photometry file.

--- txpipe/convert_desy1gold_h5py.py
+++ txpipe/convert_desy1gold_h5py.py
@@ -1,8 +1,10 @@
 from astropy.io import fits
 import h5py
 import os
+import numpy as np
 
-def add_col(array, name):
+def add_col(name):
+    array = gold_sort[name]
     total_length = len(array)
     f.create_dataset('photometry/'+name,maxshape=(total_length,), 
                      shape=(total_length,), chunks=True) 
@@ -15,25 +17,49 @@ files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
 print files[0]
 print len(files)
 
-
 # Compile all tiles into a single array
-ra, dec = [], []
-objid = []
-
+gold = {}
+gold['ra'] = []
+gold['dec'] = []
+gold['objectId'] = []
+bands = 'ugrizy'
+for band in bands:
+    gold['%s_mag'%band] = []
+    gold['%s_mag_err'%band] = []
+    
 for f in files[0:5]:
     hdu = fits.open(path+f)
-    ra.extend(hdu[1].data['RA'])
-    dec.extend(hdu[1].data['DEC'])
-    objid.extend(hdu[1].data['COADD_OBJECTS_ID'])
-    
+    d = hdu[1].data
+    gold['ra'].extend(d['RA'])
+    gold['dec'].extend(d['DEC'])
+    gold['objectId'].extend(d['COADD_OBJECTS_ID'])
+    for band in bands:
+        if band == 'u':
+            gold['%s_mag'%band].extend(d['MAG_AUTO_G'])
+            gold['%s_mag_err'%band].extend(d['MAGERR_AUTO_G'])
+        else:
+            gold['%s_mag'%band].extend(d['MAG_AUTO_%s'%band.capitalize()])
+            gold['%s_mag_err'%band].extend(d['MAGERR_AUTO_%s'%band.capitalize()])
+
+
+
+for key in gold:
+    gold[key] = np.array(gold[key])
+
+# Sort by id
+ind = np.argsort(gold['objectId'])
+gold_sort ={}
+for key in gold:
+    gold_sort[key] = gold[key][ind]
 
 # Write output into a h5py file
-outfile = 'photometry_catalog.h5py'
+outfile = 'photometry_catalog.hdf5'
 f = h5py.File(outfile, 'w')
-
-add_col(ra, 'ra')
-add_col(dec, 'dec')
-add_col(objid, 'objectId')
-
+add_col('ra')
+add_col('dec')
+add_col('objectId')
+for band in bands:
+    add_col('%s_mag'%band)
+    add_col('%s_mag_err'%band)
 f.close()
 
